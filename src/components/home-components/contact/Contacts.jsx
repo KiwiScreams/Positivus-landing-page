a02f4a06d60db9d10e5346a6fbd6e1e37a56f876
KiwiScreams <screamskiwi3@gmail.com>
2024-07-29 17:05:36 +0400

    form validation

--- src/components/home-components/contact/Contacts.jsx
+++ src/components/home-components/contact/Contacts.jsx
@@ -1,6 +1,40 @@
 import "./Contacts.css";
+import { useState, useEffect } from "react";
 import contactImage from "../../../assets/images/home/contact.svg"
 function Contacts() {
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [errors, setErrors] = useState({});
+    const [successMessage, setSuccessMessage] = useState('');
+    const name_regex = /^[a-zA-Z][a-zA-Z0-_]{3,23}$/;
+    const email_regex = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const errors = {};
+        if (!name_regex.test(name)) {
+            errors.name = 'Invalid username';
+        }
+        if (!email_regex.test(email)) {
+            errors.email = 'Invalid E-mail address';
+        }
+        if (message.trim() === '') {
+            errors.message = 'Message is required';
+        }
+        setErrors(errors);
+        if (Object.keys(errors).length === 0) {
+            setIsSubmitted(true);
+            setSuccessMessage('Message sent successfully!')
+            setTimeout(() => {
+                setIsSubmitted(false);
+                setSuccessMessage('');
+                setName('');
+                setEmail('');
+                setMessage('');
+            }, 3000);
+        }
+    }
     return (
         <>
             <section className="contacts-section">
@@ -9,7 +43,7 @@ function Contacts() {
                     <p>Connect with Us: Let's Discuss Your Digital Marketing Needs</p>
                 </div>
                 <div className="contacts-container flex">
-                    <form>
+                    <div className="form">
                         <div className="form-header flex">
                             <input type="radio" name="radio" />
                             <label htmlFor="hi">Say Hi</label>
@@ -19,19 +53,22 @@ function Contacts() {
                         <div className="form-body">
                             <div className="inp">
                                 <label htmlFor="name">Name</label>
-                                <input type="text" name="name" placeholder="Name" />
+                                <input type="text" name="name" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                                {errors.name && <div className="error">{errors.name}</div>}
                             </div>
                             <div className="inp">
                                 <label htmlFor="name">Email*</label>
-                                <input type="email" name="email" placeholder="Email" />
+                                <input type="email" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                {errors.email && <div className="error">{errors.email}</div>}
                             </div>
                             <div className="inp">
                                 <label htmlFor="name">Message*</label>
-                                <textarea placeholder="Message"></textarea>
+                                <textarea placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                                {errors.message && <div className="error">{errors.message}</div>}
                             </div>
                         </div>
-                        <button>Send Message</button>
-                    </form>
+                        <button onClick={handleSubmit}>Send Message</button>
+                    </div>
                     <div className="image-container">
                         <img src={contactImage} alt="" />
                     </div>
