11581fa1a8abf581d33f5ea4c719147d454e6a13
KiwiScreams <screamskiwi3@gmail.com>
2024-07-29 21:47:39 +0400

    header responsive

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,31 +1,35 @@
 import textLogo from "../../../assets/images/core/TextLogo.svg"
 import "./Header.css"
 import { Link } from "react-scroll"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import menu from "../../../assets/images/home/menu.svg"
 function Header() {
     const [color, setColor] = useState(false);
+    useEffect(() => {
+        const changeColor = () => {
+            if (window.scrollY >= 90) {
+                setColor(true);
+            } else {
+                setColor(false);
+            }
+        };
+        window.addEventListener('scroll', changeColor);
+        return () => {
+            window.removeEventListener('scroll', changeColor);
+        };
+    }, []);
     const [headerVisible, setHeaderVisible] = useState(false);
-    const changeColor = () => {
-        if (window.scrollY >= 90) {
-            setColor(true)
-        }
-        else {
-            setColor(false)
-        }
-    }
-    window.addEventListener('scroll', changeColor);
     const toggleHeader = () => {
         setHeaderVisible(!headerVisible);
     }
     return (
         <>
             <header className={`${color ? 'header-bg' : ''}`}>
-            <span className="menu" onClick={toggleHeader}><img src={menu} alt="" /></span>
-                <div className="header">
-                    <nav className="flex">
-                        <h1><Link to="home" spy={true} smooth={true} offset={-100} duration={500}><img src={textLogo} alt="" /></Link></h1>
-                        <ul className={`flex ${headerVisible ? 'header-show' : 'header-hide'}`}>
+                <span className="menu" onClick={toggleHeader}><img src={menu} alt="" /></span>
+                <div className="header flex">
+                    <h1><Link to="home" spy={true} smooth={true} offset={-100} duration={500}><img src={textLogo} alt="" /></Link></h1>
+                    <nav className={`flex ${headerVisible ? 'header-show' : 'header-hide'}`}>
+                        <ul className="flex">
                             <li><Link to="aboutus" spy={true} smooth={true} offset={-100} duration={500}>About us</Link></li>
                             <li><Link to="services" spy={true} smooth={true} offset={-100} duration={500}>Services</Link></li>
                             <li><Link to="cases" spy={true} smooth={true} offset={-100} duration={500}>Use Cases</Link></li>
